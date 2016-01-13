afe6f0961e1c1f74d5a0f610e12831fdb521a01d
codeamac04 <codeamac04@gmail.com>
2016-01-12 20:31:05 -0600

    first commit

--- public/js/application.js
+++ public/js/application.js
@@ -3,5 +3,51 @@ $(document).ready(function() {
 	// completamente. 
 	// Esto garantiza que si amarramos(bind) una función a un elemento 
 	// de HTML este exista ya en la página. 
+  $('.button1').click(function(e) {
+    console.log("chale valedor -fichita")
+      var conteo = 5;
+      var interval = setInterval(function(e){
+        conteo --;
+        $('#countdown').text(conteo);
+        if (conteo == 0){
+         clearInterval(interval); // stop the inte  
+        };
+      },1000);
+      // setTimeout(function(e){alert("hello") 
+         
+
+      // }, delay); 
+        
+         // $('td').removeClass('active');
+         // $(this).addClass('active');
+
+        // var $parent = $(this).parent();
+        // if (!$parent.hasClass('active')) {
+        //     $parent.addClass('active');
+        // }
+        // e.preventDefault();
+    });
 
 });
+
+// $(document).getElementById("clickMe").onclick = doFunction;
+
+// var el = document.getElementById("clickMe");
+// if (el.addEventListener)
+//   el.addEventListener("click", doFunction, false);
+// else if (el.attachEvent)
+//   el.attachEvent('onclick', doFunction);
+
+
+// $(document).ready(function () {
+//     $('.table td').click(function(e) {
+
+//         $('.table td').removeClass('active');
+
+//         var $parent = $(this).parent();
+//         if (!$parent.hasClass('active')) {
+//             $parent.addClass('active');
+//         }
+//         e.preventDefault();
+//     });
+// });
